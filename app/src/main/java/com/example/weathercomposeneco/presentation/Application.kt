30ff864be0eb91e9dd1834c898572fa6ae6dc822
AzatSabirov24 <aisabirov@alfabank.ru>
2022-12-05 11:40:18 +0300

    add Timber for logging

--- app/src/main/java/com/example/weathercomposeneco/presentation/Application.kt
+++ app/src/main/java/com/example/weathercomposeneco/presentation/Application.kt
@@ -3,6 +3,12 @@ package com.example.weathercomposeneco.presentation
 import android.app.Application
 import com.example.weathercomposeneco.di.DaggerMainComponent
 import com.example.weathercomposeneco.di.MainComponent
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.FormatStrategy
+import com.orhanobut.logger.Logger
+import com.orhanobut.logger.PrettyFormatStrategy
+import timber.log.Timber
+
 
 class App : Application() {
 
@@ -15,5 +21,21 @@ class App : Application() {
                 app = this,
                 context = this
             )
+        logging()
+    }
+
+    private fun logging() {
+        val formatStrategy: FormatStrategy = PrettyFormatStrategy.newBuilder()
+            .showThreadInfo(true)
+            .methodCount(1)
+            .methodOffset(5)
+            .tag("")
+            .build()
+        Logger.addLogAdapter(AndroidLogAdapter(formatStrategy))
+        Timber.plant(object : Timber.DebugTree() {
+            override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                Logger.log(priority, "-$tag", message, t)
+            }
+        })
     }
 }
